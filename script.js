bdd06d25d4bba4d4d7ee5b844353d21d3cf150fd
a-phu <annabel.phu@hotmail.com>
2021-12-24 12:09:54 +1100

    updated basic functionality for all buttons - can play a game of rps until player or computer gets 5 pts

--- script.js
+++ script.js
@@ -37,19 +37,70 @@ compPtsP.textContent = compPts;
 //moved separate function into event listener function for rock button
 //updates result and player + comp pts
 rockButton.addEventListener('click', () => {
-    let computerSelection = computerPlay();
-    let result = '';
-    if(computerSelection === "rock"){
-        result = "It's a tie!";
-    } else if (computerSelection === "paper"){
-        compPts++;
-        result =  "You lose! Paper beats Rock.";
-    } else if (computerSelection === "scissors"){ 
-        playerPts++;
-        result =  "You win! Rock beats Scissors.";
+    playGame("rock");
+});
+paperButton.addEventListener('click', () => {
+    playGame("paper");
+});
+scissorsButton.addEventListener('click', () => {
+    playGame("scissors");
+});
+
+
+//function to play rps passed to each button
+function playGame(playerSelection){
+    if(playerPts < 5 && compPts < 5){
+        let computerSelection = computerPlay();
+        let result = '';
+        switch(playerSelection){
+            case "rock":
+                if(computerSelection === "rock"){
+                    result = "It's a tie!";
+                } else if (computerSelection === "paper"){
+                    compPts++;
+                    result =  "You lose! Paper beats Rock.";
+                } else if (computerSelection === "scissors"){ 
+                    playerPts++;
+                    result =  "You win! Rock beats Scissors.";
+                }
+                break;
+            case "paper":
+                if(computerSelection === "rock"){
+                    playerPts++;
+                    result = "You win! Paper beats Rock.";
+                } else if (computerSelection === "paper"){
+                    result = "It's a tie!";
+                } else if (computerSelection === "scissors"){
+                    compPts++;
+                    result = "You lose! Scissors beats Paper.";
+                }
+                break;
+            case "scissors":
+                if(computerSelection === "rock"){
+                    compPts++;
+                    result = "You lose! Rock beats Scissors.";
+                } else if (computerSelection === "paper"){
+                    playerPts++;
+                    result = "You win! Scissors beats Paper.";
+                } else if (computerSelection === "scissors"){
+                    result = "It's a tie!";
+                }
+                break;
+        }
+        //update result
+        resultsDiv.textContent = result;
+
+        //update running total player and comp pts
+        playerPtsP.textContent = playerPts;
+        compPtsP.textContent = compPts;
     }
 
-    resultsDiv.textContent = result;
-    playerPtsP.textContent = playerPts;
-    compPtsP.textContent = compPts;
-});
+    if(playerPts == 5){
+        resultsDiv.textContent = "You have beaten the computer!";
+    } else if (compPts == 5){
+        resultsDiv.textContent = "You have lost against the computer! Better luck next time!";
+    }
+    
+}
+
+
